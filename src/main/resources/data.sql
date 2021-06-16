75de2d2ceb6fe71582ab3e44864907aa1e917fa2
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-16 16:38:58 +0200

    Insert data to db

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dog (id long, name varchar(255), breed varchar(255), origin varchar(255));
+
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'Ninja', 'Husky', 'Sarajevo');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'Spot', 'Pit Bull', 'Amsterdam');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'Mongo', 'Cocker Spaniel', 'Berlin');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'Lady', 'Direwolf', 'Torino');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'Kapa', 'Golden Retriever', 'Zagreb');
