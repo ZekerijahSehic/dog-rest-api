ff0b049b8da2eabfb38eeb078b5414236e6e2284
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-16 16:55:24 +0200

    First endpoint

--- src/main/java/com/zekerijah/dogrestapi/controller/DogController.java
+++ src/main/java/com/zekerijah/dogrestapi/controller/DogController.java
@@ -1,7 +1,26 @@
 package com.zekerijah.dogrestapi.controller;
 
+import com.zekerijah.dogrestapi.entity.Dog;
+import com.zekerijah.dogrestapi.services.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DogController {
+
+    @Autowired
+    private DogService dogService;
+
+    @GetMapping("/dogs")
+    public ResponseEntity<List<Dog>> getAllDogs(){
+        List<Dog> dogList = dogService.getDogs();
+        return new ResponseEntity<List<Dog>>(dogList, HttpStatus.OK);
+    }
+
+
 }
