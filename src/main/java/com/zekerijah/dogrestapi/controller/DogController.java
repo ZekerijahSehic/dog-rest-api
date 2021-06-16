c8d5cc7a8cc86647977a43f330918823d32d330d
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-16 17:05:50 +0200

    Endpoints created

--- src/main/java/com/zekerijah/dogrestapi/controller/DogController.java
+++ src/main/java/com/zekerijah/dogrestapi/controller/DogController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Locale;
 
 @RestController
 public class DogController {
@@ -20,6 +22,24 @@ public class DogController {
     public ResponseEntity<List<Dog>> getAllDogs(){
         List<Dog> dogList = dogService.getDogs();
         return new ResponseEntity<List<Dog>>(dogList, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/breed")
+    public ResponseEntity<List<String>> getDogBreeds(){
+        List<String> dogBreedsList = dogService.getDogBreed();
+        return new ResponseEntity<List<String>>(dogBreedsList, HttpStatus.OK);
+    }
+
+    @GetMapping("/breed/{id}")
+    public ResponseEntity<String> getBreedById(@PathVariable Long id){
+        String breed = dogService.getDogByBreedId(id);
+        return new ResponseEntity<String>(breed, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/names")
+    public ResponseEntity<List<String>> getDogNames(){
+        List<String> dogNames = dogService.getDogNames();
+        return new ResponseEntity<List<String>>(dogNames, HttpStatus.OK);
     }
 
 
