6f431f8d1c6e64e555b6bc5754430aa3155baf5b
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-16 15:58:41 +0200

    Change data types

--- src/main/java/com/zekerijah/dogrestapi/repository/DogRepository.java
+++ src/main/java/com/zekerijah/dogrestapi/repository/DogRepository.java
@@ -11,12 +11,12 @@ import java.util.List;
 public interface DogRepository extends CrudRepository<Dog, Long> {
 
     @Query("select dog.id, dog.name from Dog dog")
-    List<Dog> findAllByName();
+    List<String> findAllByName();
 
     @Query("select dog.id, dog.breed from Dog dog")
-    List<Dog> findAllByBreed();
+    List<String> findAllByBreed();
 
     @Query("select dog.id, dog.breed from Dog dog where dog.id=:id")
-    String findBreedById();
+    String findBreedById(Long id);
 
 }
