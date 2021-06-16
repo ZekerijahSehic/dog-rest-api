a6136d4573171d595502fa7fa689771a43202cb3
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-16 14:37:47 +0200

    Repository

---
+++ src/main/java/com/zekerijah/dogrestapi/repository/DogRepository.java
@@ -0,0 +1,22 @@
+package com.zekerijah.dogrestapi.repository;
+
+import com.zekerijah.dogrestapi.entity.Dog;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DogRepository extends CrudRepository<Dog, Long> {
+
+    @Query("select dog.id, dog.name from Dog dog")
+    List<Dog> findAllByName();
+
+    @Query("select dog.id, dog.breed from Dog dog")
+    List<Dog> findAllByBreed();
+
+    @Query("select dog.id, dog.breed from Dog dog where dog.id=:id")
+    String findBreedById();
+
+}
