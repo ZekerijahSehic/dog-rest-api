9e73c2ec9d434549a4d15f466c109e185ba1cfc6
Zekerijah <zekerijahsehic1@gmail.com>
2021-06-16 16:40:31 +0200

    Lombok import

--- src/main/java/com/zekerijah/dogrestapi/entity/Dog.java
+++ src/main/java/com/zekerijah/dogrestapi/entity/Dog.java
@@ -1,10 +1,12 @@
 package com.zekerijah.dogrestapi.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Data
 @Entity
 public class Dog {
     @Id
@@ -15,50 +17,5 @@ public class Dog {
     private String breed;
     private String origin;
 
-    public Dog(Long id, String name, String breed, String origin) {
-        this.id = id;
-        this.name = name;
-        this.breed = breed;
-        this.origin = origin;
-    }
 
-    public Dog(String name, String breed) {
-        this.name = name;
-        this.breed = breed;
-    }
-
-    public Dog() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getBreed() {
-        return breed;
-    }
-
-    public void setBreed(String breed) {
-        this.breed = breed;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    public void setOrigin(String origin) {
-        this.origin = origin;
-    }
 }
